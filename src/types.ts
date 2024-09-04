2a8f7d507a03e1432705497b5765dc2309ddce73
gemrey13 <gemreyranola@gmail.com>
2024-09-04 23:53:23 +0800

    ci: adding some context to the codebase

---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export type Data = {
+  amount: number;
+  description: string;
+  remarks: string | undefined;
+};
--- src/variables.ts
+++ src/variables.ts
@@ -1,7 +1,1 @@
 export const api: string = "https://api.paymongo.com/v1/links";
-
-export type Data = {
-  amount: number;
-  description: string;
-  remarks: string | undefined;
-};
